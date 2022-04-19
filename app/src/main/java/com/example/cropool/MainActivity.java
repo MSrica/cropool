3cbe172ba25132f553db818513af17f77c9c3936
Ivan Rubinić <ivan.rubinic@outlook.com>
2022-04-20 01:10:44 +0200

    Added splash screen to MainActivity
    Added the splash screen by editing theme files (for day and night mode) and AndroidManifest. Splash screen is only active if MainActivity is not ready (splash screen can be tested uncommenting sleep command which will be removed before the PR).

--- app/src/main/java/com/example/cropool/MainActivity.java
+++ app/src/main/java/com/example/cropool/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.cropool;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        Uncomment for testing the splash screen
+//        try {
+//            TimeUnit.SECONDS.sleep(3);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        this.setTheme(R.style.Theme_Cropool);
+
         setContentView(R.layout.activity_main);
     }
 }
