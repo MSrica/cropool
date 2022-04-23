34c71a3675004ca59540467fe490b65ebd8ab920
Ivan Rubinić <ivan.rubinic@outlook.com>
2022-04-23 23:14:39 +0200

    Login screen .xml file finished; #4
    Login screen was designed according to the given template. Link from
    Welcome screen to Login screen via Sign In button was also made.

--- app/src/main/java/com/example/cropool/LoginFragment.java
+++ app/src/main/java/com/example/cropool/LoginFragment.java
@@ -4,33 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-public class WelcomeFragment extends Fragment {
-    public WelcomeFragment() {
+public class LoginFragment extends Fragment {
+    public LoginFragment() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
-
-        Button signIn = view.findViewById(R.id.sign_in);
-
-        signIn.setOnClickListener(v -> {
-            LoginFragment loginFragment = new LoginFragment();
-            requireActivity().getSupportFragmentManager().beginTransaction().add(R.id.main_activity_fragment_container, loginFragment).addToBackStack(null).commit();
-        });
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
 
         return view;
     }
--- app/src/main/java/com/example/cropool/MainActivity.java
+++ app/src/main/java/com/example/cropool/MainActivity.java
@@ -3,22 +3,12 @@ package com.example.cropool;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        Uncomment for testing the splash screen
-//        try {
-//            TimeUnit.SECONDS.sleep(3);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
 
         this.setTheme(R.style.Theme_Cropool);
 
